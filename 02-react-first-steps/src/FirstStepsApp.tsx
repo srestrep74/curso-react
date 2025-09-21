fe1b0fa0719c17a058e40b2441c625c37aab83a5
sebastian <srestrep74@eafit.edu.co>
2025-09-21 13:34:07 -0500

    feat: learning basic of jsx and expressions

--- 02-react-first-steps/src/FirstStepsApp.tsx
+++ 02-react-first-steps/src/FirstStepsApp.tsx
@@ -1,14 +1,35 @@
 export function FirstStepsApp() {
+
+    // Variables y constantes (tipadas porque estás en TS)
+    const firstName: string = 'Sebastian';
+    const lastName: string = 'Restrepo';
+
+    const favoriteGames: string[] = [
+        "Fifa",
+        "God of war",
+        "GTA"
+    ];
+
+    const isActive: boolean = false;
+
+    /*
+     Nota: Si estas variables fueran valores constantes
+     que nunca cambian entre renderizados, sería más eficiente
+     definirlas fuera de la función del componente.
+     */
+
     return (
         <>
             <h1>Hola Mundo</h1>
-            <p>Esto es un parrafo</p>
 
-            <button>Click me</button>
+            {/* 🔹 Interpolación de variables */}
+            <p>Esto es mi nombre: {firstName} {lastName}</p>
 
-            <div>
-                <h2>Hola dentro de un div</h2>
-            </div>
+            {/* 🔹 Usar lógica de JS dentro de JSX */}
+            <p>Mis juegos favoritos: {favoriteGames.join(', ')}</p>
+
+            {/* 🔹 Operador ternario */}
+            <h3>{isActive ? 'Activo ✅' : 'No activo ❌'}</h3>
         </>
     );
 }
