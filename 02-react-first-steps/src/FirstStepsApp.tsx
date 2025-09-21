af11d8c18a675cd8f6eea5eb9633f93af8e5d9a5
sebastian <srestrep74@eafit.edu.co>
2025-09-21 14:12:03 -0500

    feat: learning basic of css on componentes

--- 02-react-first-steps/src/FirstStepsApp.tsx
+++ 02-react-first-steps/src/FirstStepsApp.tsx
@@ -1,3 +1,11 @@
+import type { CSSProperties } from "react";
+
+interface Game {
+    id: number;
+    title: string;
+    description: string;
+}
+
 export function FirstStepsApp() {
 
     // Variables y constantes (tipadas porque estás en TS)
@@ -12,11 +20,25 @@ export function FirstStepsApp() {
 
     const isActive: boolean = false;
 
+    const listGames: Game[] = [
+        {id: 1, title: "Fifa", description: "Soccer game"},
+        {id: 2, title: "God of war", description: "Fight game"},
+        {id: 3, title: "GTA", description: "Open world game"}
+    ];
+
+
     /*
      Nota: Si estas variables fueran valores constantes
      que nunca cambian entre renderizados, sería más eficiente
      definirlas fuera de la función del componente.
      */
+
+    const myStyles: CSSProperties = {
+        display: "flex",
+        flexDirection: "row",
+        padding: 10,
+        justifyContent: "space-between"
+    };
 
     return (
         <>
@@ -30,6 +52,22 @@ export function FirstStepsApp() {
 
             {/* 🔹 Operador ternario */}
             <h3>{isActive ? 'Activo ✅' : 'No activo ❌'}</h3>
+
+            <div style={myStyles}>
+                {listGames.map(game => (
+                    <div key={game.id}>
+                        <h1>Title: {game.title}</h1>
+                        <p
+                            style ={{
+                                backgroundColor: "lightblue",
+                                borderRadius: isActive ? 10 : 20,
+                            }}
+                        >
+                            Description: {game.description}
+                        </p>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
